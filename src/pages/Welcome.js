4efcc7a322231acb881dd4059f8be61ed36b882e
Pedro Barros <pedrobars7a@gmail.com>
2021-11-12 18:26:27 -0300

    add images

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,10 +1,39 @@
 import React from "react";
-import { Text, View } from "react-native";
+import Logo from "../assets/img/logo.png";
+import Card from "../assets/img/cardimg.png";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Alert,
+  SafeAreaView,
+  Dimensions
+} from "react-native";
 
 export default function Welcome() {
   return (
-    <View>
-      <Text>Welcome</Text>
-    </View>
+    <SafeAreaView style={styled.container}>
+      <Image style={styled.image} source={Logo} resizeMode="contain" />
+      <Image style={styled.card} source={Card} resizeMode="contain" />
+    </SafeAreaView>
   );
 }
+
+const styled = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+    alignItems: "center"
+  },
+  image: {
+    width: 150
+  },
+  text: {
+    color: "#fff"
+  },
+  card: {
+    height: Dimensions.get("window").width * 0.6
+  }
+});
